049afbebfa11ebfc402344409243aafc103d8b6a
Dan Fulea <fulea.dan@gmail.com>
2018-12-06 20:57:44 +0200

    Fixed the 10 degrees anode angle bug in XRay module. Updated gitignore
    Signed-off-by: Dan Fulea <fulea.dan@gmail.com>

--- src/radQC/resources/RadQCFrameResources.java
+++ src/radQC/resources/RadQCFrameResources.java
@@ -65,7 +65,7 @@ public class RadQCFrameResources extends ListResourceBundle{
 			{ "Author.name", "Dan Fulea , fulea.dan@gmail.com" },
 
 			{ "Version", "Version:" },
-			{ "Version.name", "RadQC 1.2" },
+			{ "Version.name", "RadQC 1.3" },
 
 			{
 					"License",
